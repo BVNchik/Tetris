4055c6b255938810e42d54181c2c40f6455ddc7c
Unknown <bulanov.vladislav32@gmail.com>
2018-01-19 17:53:41 +0300

    added update button, changed queries, optimized code, added geolocation detection

---
+++ app/src/main/java/ru/kodep/vlad/weather/DBHelper.java
@@ -0,0 +1,8 @@
+package ru.kodep.vlad.weather;
+
+/**
+ * Created by vlad on 16.01.18.
+ */
+
+public class DBHelper {
+}
---
+++ app/src/main/java/ru/kodep/vlad/weather/GeoLocation.java
@@ -0,0 +1,8 @@
+package ru.kodep.vlad.weather;
+
+/**
+ * Created by vlad on 18.01.18.
+ */
+
+public class GeoLocation {
+}
