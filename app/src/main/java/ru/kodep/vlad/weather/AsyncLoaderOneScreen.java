25aa4505c4c02f3f4cba08885a29b47ab4f7c54a
Unknown <bulanov.vladislav32@gmail.com>
2018-02-19 16:28:02 +0300

    added work with the fragment, MainActivity now the fragment manager, a working application

---
+++ app/src/main/java/ru/kodep/vlad/weather/AsyncLoaderOneScreen.java
@@ -0,0 +1,41 @@
+package ru.kodep.vlad.weather;
+
+        import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.content.CursorLoader;
+
+import ru.kodep.vlad.weather.entity.GuestProvaider;
+
+/**
+ * Created by vlad on 24.01.18
+ */
+
+public class AsyncLoaderOneScreen  extends  CursorLoader{
+
+    private GuestProvaider guestProvaider;
+    @SuppressLint("StaticFieldLeak")
+    private Context context;
+    @SuppressLint("StaticFieldLeak")
+    AsyncLoaderOneScreen(Context context, GuestProvaider guestProvaider) {
+        super(context);
+        this.context = context;
+        this.guestProvaider = guestProvaider;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public Cursor loadInBackground() {
+        String mCity = new CityPreference((Activity) context).getCity();
+        String selection = "cityname = ?";
+        String[] selectionArgs = new String[]{mCity};
+        return guestProvaider.query(selection, selectionArgs);
+    }
+}
